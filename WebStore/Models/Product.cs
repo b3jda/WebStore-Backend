846966c8b02fc6e9d8f0345d755d25a31eb81d62
Bejda Maksuti <bejda.maksuti@gmail.com>
2025-02-04 02:11:45 +0100

    Solr

--- WebStore/Models/Product.cs
+++ WebStore/Models/Product.cs
@@ -1,47 +1,72 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
+using SolrNet.Attributes;
 
 namespace WebStore.Models
 {
     public class Product
     {
         [Key]
+        [SolrUniqueKey("id")]
         public int Id { get; set; }
+
+        [SolrField("name")]
         public string Name { get; set; }
+
+        [SolrField("description")]
         public string Description { get; set; }
+
+        [SolrField("price")]
         public decimal Price { get; set; }
+
+        [SolrField("quantity")]
         public int Quantity { get; set; }
+
+        [SolrField("original_price")]
         public decimal OriginalPrice { get; set; }
-        public bool InStock => Quantity > 0;
 
-        // Foreign keys and relationships
-        [ForeignKey("Category")]
-        public int CategoryId { get; set; }
-        public Category Category { get; set; }
-
-        [ForeignKey("Brand")]
-        public int BrandId { get; set; }
-        public Brand Brand { get; set; }
-
-        [ForeignKey("Gender")]
-        public int GenderId { get; set; }
-        public Gender Gender { get; set; }
-
-        [ForeignKey("Color")]
-        public int ColorId { get; set; }
-        public Color Color { get; set; }
-
-        [ForeignKey("Size")]
-        public int SizeId { get; set; }
-        public Size Size { get; set; }
-
-        //  One discount can apply at a time
-        [ForeignKey("Discount")]
-        public int? DiscountId { get; set; }
-        public Discount? Discount { get; set; }
-
+        [SolrField("is_discounted")]
         public bool IsDiscounted { get; set; }
+
+        [SolrField("discount_percentage")]
         public double? DiscountPercentage { get; set; }
 
+        // Foreign Key Properties (IDs)
+        [SolrField("category_id")]
+        public int CategoryId { get; set; }
+
+        [SolrField("brand_id")]
+        public int BrandId { get; set; }
+
+        [SolrField("gender_id")]
+        public int GenderId { get; set; }
+
+        [SolrField("color_id")]
+        public int ColorId { get; set; }
+
+        [SolrField("size_id")]
+        public int SizeId { get; set; }
+
+        [SolrField("discount_id")]
+        public int? DiscountId { get; set; }
+
+        //  Foreign Key Attributes (Explicitly Specify the Property Name)
+        [ForeignKey(nameof(CategoryId))]
+        public Category Category { get; set; }
+
+        [ForeignKey(nameof(BrandId))]
+        public Brand Brand { get; set; }
+
+        [ForeignKey(nameof(GenderId))]
+        public Gender Gender { get; set; }
+
+        [ForeignKey(nameof(ColorId))]
+        public Color Color { get; set; }
+
+        [ForeignKey(nameof(SizeId))]
+        public Size Size { get; set; }
+
+        [ForeignKey(nameof(DiscountId))]
+        public Discount? Discount { get; set; }
     }
 }
